b4064096d1888dee8f3cc3fefbc9093137ba9577
Test1 1 <williamh2461@gmail.com>
2022-12-06 01:45:30 -0500

    added routes to app.js and created pages folder

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  return  <div className="App"></div>
- 
+  return ( 
+  <div className="App">
+    <Router> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+      </Routes>
+    </Router>
+
+
+  </div>
+  )
 }
 
 export default App;
