469f53c2c783d38d0bacb40fefa0488e7bcab977
Test1 1 <williamh2461@gmail.com>
2022-12-26 19:51:19 -0500

    added a footer to the page, added social icons

---
+++ my-portfolio/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div>
+        <FontAwesomeIcon icon="fa-brands fa-instagram" />
+        <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+        <FontAwesomeIcon icon="fa-brands fa-github" />
+        <FontAwesomeIcon icon="fa-solid fa-envelope" />
+        </div>
+        <p> &copy; 2022 Liamh.com </p>
+      
+    </div>
+  )
+}
+
+export default Footer
